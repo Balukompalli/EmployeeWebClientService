a855ca5d2099be39b3a0bca37fc771dd6fa3e079
Prasad <balumail2010@gmail.com>
2023-11-14 18:21:23 +0530

    refactored and re formatted the code

--- src/main/java/com/employee/webclient/controller/EmployeeWebclientController.java
+++ src/main/java/com/employee/webclient/controller/EmployeeWebclientController.java
@@ -2,9 +2,6 @@ package com.employee.webclient.controller;
 
 import com.employee.webclient.model.EmployeeRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpRequest;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
@@ -26,9 +23,9 @@ public class EmployeeWebclientController {
 //    }
 
     @PostMapping("/saveEmployee")
- public Mono<String>  saveEmployee(@RequestBody EmployeeRequest employeeRequest) {
-    return webClient.post().uri("/saveEmployee").syncBody(employeeRequest).retrieve().bodyToMono(String.class);
- }
+    public Mono<String> saveEmployee(@RequestBody EmployeeRequest employeeRequest) {
+        return webClient.post().uri("/saveEmployee").syncBody(employeeRequest).retrieve().bodyToMono(String.class);
+    }
 
     @GetMapping("/getEmployees")
     public Flux<EmployeeRequest> getEmployee() {
@@ -39,7 +36,7 @@ public class EmployeeWebclientController {
     @GetMapping("/getEmployeeById")
     public Mono<EmployeeRequest> getEmployeeById(@RequestParam("employeeId") Long employeeId) {
         return webClient.get()
-                .uri("/getEmployeeById?employeeId="+employeeId)
+                .uri("/getEmployeeById?employeeId=" + employeeId)
                 .retrieve().bodyToMono(EmployeeRequest.class);
     }
 
--- src/test/java/com/employee/webclient/EmployeeWebClientServiceApplicationTests.java
+++ src/test/java/com/employee/webclient/EmployeeWebClientServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EmployeeWebClientServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
